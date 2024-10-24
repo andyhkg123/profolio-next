08cc6970585af3f2008a98194ac1fc1fb9ac6a85
Andy Wong <andy.wongtszkiu@gmail.com>
2024-10-25 01:47:10 +0800

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,9 @@ const Footer = () => {
 
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="#" class="hover:underline me-4 md:me-6">
+              <div href="#" class="hover:underline me-4 md:me-6">
                 This profolio is created by NextJS & MongoDB
-              </a>
+              </div>
             </li>
           </ul>
         </div>
