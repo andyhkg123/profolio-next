3c26b106cb2e272ec26c311837f249843da14a6c
Andy Wong <andy.wongtszkiu@gmail.com>
2024-10-28 18:12:16 +0800

    text

--- src/pages/Name.jsx
+++ src/pages/Name.jsx
@@ -20,6 +20,7 @@ const Home = () => {
             px-8
             py-24
             text-black
+            dark:text-cyan-900
           >
             <FlipLink href="#">Andy</FlipLink>
             <br />
@@ -28,7 +29,7 @@ const Home = () => {
 
           <br />
           <div>
-            <span className=" text-2xl typewriter bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+            <span className=" text-2xl typewriter bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text dark:text-cyan-900">
               {/* Full Stack Developer */}
             </span>
           </div>
